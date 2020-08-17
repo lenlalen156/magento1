e809873acc6fe9a091ccd0fa7487b1ff023c90f3
manhnv <manhnv@2nf.com.vn>
2020-08-17 16:51:10 +0600

    17/08/2020: create new page admin: hien thi tren menu, page thi chua

--- app/etc/config.php
+++ app/etc/config.php
@@ -262,6 +262,7 @@ return [
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
         'Temando_ShippingRemover' => 1,
+        'Theme_Module' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
         'Yotpo_Yotpo' => 1
